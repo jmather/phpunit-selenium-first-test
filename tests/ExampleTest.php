e8c69449e2196c9645ef9bb2d0e2faaf146cd956
Jacob Mather <jmather@jmather.com>
2012-11-29 22:19:52 -0500

    Initial import

---
+++ tests/ExampleTest.php
@@ -0,0 +1,29 @@
+<?php
+
+class ExampleTest extends PHPUnit_Extensions_Selenium2TestCase
+{
+    public $browser = '*firefox';
+
+    public function setUp()
+    {
+        $this->setBrowser($this->browser);
+        $this->setBrowserUrl('http://jmather.com/');
+    }
+
+    public function testTitle()
+    {
+        $this->url('/');
+        $title = 'It\'s Majax';
+        $this->assertEquals($title, $this->title());
+    }
+
+    public function testSomeNavigation()
+    {
+        $this->url('/');
+        $this->byXPath('//section[@class="widget widget_categories"]//a[text() = "PHP"]')->click();
+        $title = 'PHP | It\'s Majax';
+        $this->assertEquals($title, $this->title());
+    }
+
+}
+
